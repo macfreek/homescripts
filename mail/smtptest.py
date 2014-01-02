1155da07ff8d8cd4be652fab1b7798c47adce234
Freek Dijkstra <freek@macfreek.nl>
2014-01-02 21:48:47 +0100

    Organise script in functions and methods

--- mail/smtptest.py
+++ mail/smtptest.py
@@ -11,36 +11,14 @@ import optparse
 
 try:
     from dns.resolver import query, NoNameservers
-    from dns.name import from_text as dns_from_text
-    def dns_encode(domain):
-        return str(dns_from_text(domain))[:-1]
 except ImportError:
     def query(qname, rdtype):
         return []
-    def dns_encode(domain):
-        labels = []
-        for label in domain.split('.'):
-            try:
-                labels.append(label)
-            except UnicodeError:
-                labels.append('xn--' + label.encode('punycode').decode('ascii'))
-        return ".".join(labels)
 
 try:
     input = raw_input  # Python 2.x compatibility
 except NameError:
     pass
-
-default_from = None
-
-parser = optparse.OptionParser()
-parser.add_option("-f", "--from", dest="fromaddr", help="From mail address")
-parser.add_option("-t", "--to", dest="toaddrs", help="To mail address. May be used multiple times.", action="append", default=[])
-parser.add_option("-s", "--server", dest="toserver", help="Delivery server")
-parser.add_option("--tls", dest="usetls", default=False, action="store_true", help="Use TLS encryption")
-parser.add_option("-u", "--user", dest="username", help="Username for authentication")
-parser.add_option("-p", "--password", dest="password", help="Password for authentication")
-(options, args) = parser.parse_args()
 
 class SMTP(smtplib.SMTP):
     """Override of smtplib.SMTP to support international email addresses according to RFC 6531."""
@@ -78,91 +56,116 @@ def get_mx_server(email):
         return str(mx_rr[0].exchange)
     return None
 
-def email_encode(emailaddress):
+def email_encode(emailaddress, has_smtputf8=False):
     """Given a Unicode email address, return a UTF-8/Punycode encoded byte"""
     user, domain = emailaddress.rsplit('@', 1)
-    return user + '@' + dns_encode(domain)
+    if has_smtputf8:
+        return user + '@' + domain.encode('idna').decode('ascii')
+    else:
+        return user + '@' + domain.encode('idna').decode('ascii')
 
-try:
-    if options.fromaddr != None:
-        fromaddr = options.fromaddr
-    else:
-        if default_from == None:
-            default_from = "postmaster@" + thisserver
-        fromaddr = prompt("From [%s]: " % (default_from))
-        if fromaddr == "":
-            fromaddr = default_from
-    if len(options.toaddrs) > 0:
-        toaddrs = options.toaddrs
-    else:
-        toaddrs  = prompt("To (separate multiple to addresses with a space): ").split()
-        if len(toaddrs) == 0:
-            print("No addresses specified")
-            raise KeyboardInterrupt
-    if options.toserver != None:
-        toserver = options.toserver
-    else:
-        mxserver = get_mx_server(toaddrs[0])
-        if options.username:
-            mxserver += ":587"
-        if mxserver is not None:
-            toserver = prompt("Server [%s]: " % mxserver)
-            if toserver == "":
-                toserver = mxserver
+def send_mail(fromaddr, toaddrs, toserver, usetls=False, username=None, password=None):
+    # TODO: use US_ASCII encoding in the headers.
+    # Add the From: and To: headers at the start!
+    headers = ("From: %s\r\nTo: %s\r\nSubject: test mail %s\r\nContent-Type: text/plain; charset=\"utf-8\"\r\n\r\n"
+           % (fromaddr, ", ".join(toaddrs), time.strftime('%y%m%d %H:%M:%S')))
+    body = ("This is a test email.\r\n\r\nFrom: %s\r\nOriginating server: %s\r\nTo: %s\r\nServer: %s\r\n"
+           % (fromaddr, thisserver, ", ".join(toaddrs), toserver))
+
+    msg = headers + body
+
+    print("Message length is %d" % len(msg))
+
+    try:
+        print("connecting to %s" % toserver)
+        server = SMTP(toserver)
+        print("sending EHLO message")
+        server.set_debuglevel(1)
+        if usetls:
+            server.starttls()
+        server.ehlo(thisserver)
+        if username:
+            # Optional: SASL authentication
+            server.login(username, password)
+        esmtp_opts = []
+        if server.has_extn('size'):
+            esmtp_opts.append("SIZE=%d" % len(msg))
+        if server.has_extn('8bitmime'):
+            esmtp_opts.append('BODY=8BITMIME')
+        if server.has_extn('smtputf8'):
+            has_smtputf8 = True
+            esmtp_opts.append('SMTPUTF8')
         else:
-            toserver = prompt("Server: ")
-except KeyboardInterrupt:
-    print("Aborted")
-    sys.exit(1)
-
-# TODO: use US_ASCII encoding in the headers.
-# Add the From: and To: headers at the start!
-headers = ("From: %s\r\nTo: %s\r\nSubject: test mail %s\r\nContent-Type: text/plain; charset=\"utf-8\"\r\n\r\n"
-       % (fromaddr, ", ".join(toaddrs), time.strftime('%y%m%d %H:%M:%S')))
-body = ("This is a test email.\r\n\r\nFrom: %s\r\nOriginating server: %s\r\nTo: %s\r\nServer: %s\r\n"
-       % (fromaddr, thisserver, ", ".join(toaddrs), toserver))
-
-msg = headers + body
-
-print("Message length is %d" % len(msg))
-
-try:
-    print("connecting to %s" % toserver)
-    server = SMTP(toserver)
-    print("sending EHLO message")
-    server.set_debuglevel(1)
-    if options.usetls:
-        server.starttls()
-    server.ehlo(thisserver)
-    if options.username:
-        # Optional: SASL authentication
-        server.login(options.username, options.password)
-    esmtp_opts = []
-    if server.has_extn('size'):
-        esmtp_opts.append("SIZE=%d" % len(msg))
-    if server.has_extn('8bitmime'):
-        esmtp_opts.append('BODY=8BITMIME')
-    if server.has_extn('smtputf8'):
-        esmtp_opts.append('SMTPUTF8')
-    (code, resp) = server.mail(fromaddr, esmtp_opts)
-    if code != 250:
-        raise smtplib.SMTPSenderRefused(code, resp, fromaddr)
-
-    for addr in toaddrs:
-        # Note: only supports IDN (part after the @), not yet international email (part before the @)
-        enc_addr = email_encode(addr)
-        print("Adding recipient %s (as %s)" % (addr, enc_addr))
-        (code, resp) = server.rcpt(enc_addr)
-        if code not in (250, 251):
-            raise smtplib.SMTPRecipientsRefused(resp)
+            has_smtputf8 = False
+        enc_fromaddr = email_encode(fromaddr, has_smtputf8)
+        (code, resp) = server.mail(fromaddr, esmtp_opts)
+        if code != 250:
+            raise smtplib.SMTPSenderRefused(code, resp, fromaddr)
     
-    (code, resp) = server.data(msg.encode('utf-8'))
-    if code != 250:
-        raise smtplib.SMTPDataError(code, resp)
+        for addr in toaddrs:
+            # Note: only supports IDN (part after the @), not yet international email (part before the @)
+            enc_addr = email_encode(addr, has_smtputf8)
+            if addr == enc_addr:
+                print("Adding recipient %s" % (enc_addr))
+            else:
+                print("Adding recipient %s (as %s)" % (addr, enc_addr))
+            (code, resp) = server.rcpt(enc_addr)
+            if code not in (250, 251):
+                raise smtplib.SMTPRecipientsRefused(resp)
     
-except smtplib.SMTPException as e:
-    print(str(e))
-finally:
-    server.quit()
+        (code, resp) = server.data(msg.encode('utf-8'))
+        if code != 250:
+            raise smtplib.SMTPDataError(code, resp)
+    
+    except smtplib.SMTPException as e:
+        print(str(e))
+    finally:
+        server.quit()
 
+if __name__ == "__main__":
+    default_from = None
 
+    parser = optparse.OptionParser()
+    parser.add_option("-f", "--from", dest="fromaddr", help="From mail address")
+    parser.add_option("-t", "--to", dest="toaddrs", help="To mail address. May be used multiple times.", action="append", default=[])
+    parser.add_option("-s", "--server", dest="toserver", help="Delivery server")
+    parser.add_option("--tls", dest="usetls", default=False, action="store_true", help="Use TLS encryption")
+    parser.add_option("-u", "--user", dest="username", help="Username for authentication")
+    parser.add_option("-p", "--password", dest="password", help="Password for authentication")
+    (options, args) = parser.parse_args()
+    
+    try:
+        if options.fromaddr != None:
+            fromaddr = options.fromaddr
+        else:
+            if default_from == None:
+                default_from = "postmaster@" + thisserver
+            fromaddr = prompt("From [%s]: " % (default_from))
+            if fromaddr == "":
+                fromaddr = default_from
+        if len(options.toaddrs) > 0:
+            toaddrs = options.toaddrs
+        else:
+            toaddrs  = prompt("To (separate multiple to addresses with a space): ").split()
+            if len(toaddrs) == 0:
+                print("No addresses specified")
+                raise KeyboardInterrupt
+        if options.toserver != None:
+            toserver = options.toserver
+        else:
+            mxserver = get_mx_server(toaddrs[0])
+            if options.username:
+                mxserver += ":587"
+            if mxserver is not None:
+                toserver = prompt("Server [%s]: " % mxserver)
+                if toserver == "":
+                    toserver = mxserver
+            else:
+                toserver = prompt("Server: ")
+        
+        send_mail(fromaddr, toaddrs, toserver, options.usetls, options.username, options.password)
+        
+        sys.exit(0)
+    except KeyboardInterrupt:
+        print("Aborted")
+        sys.exit(1)
